f4a0233d8be77923bf6847e06319c63045d21f63
Andy Shih <andyshih12@gmail.com>
2023-04-28 02:04:55 -0700

    add parallel ddim and dpmsolver

--- muse/models/diffusion/schedulers/batch_ddpm_scheduler.py
+++ muse/models/diffusion/schedulers/batch_ddpm_scheduler.py
@@ -4,43 +4,6 @@ from typing import List, Tuple, Union
 import torch
 
 class BatchDDPMScheduler(DDPMScheduler):
-    
-    def _batch_get_variance(self, t, predicted_variance=None, variance_type=None):
-        num_inference_steps = self.num_inference_steps if self.num_inference_steps else self.config.num_train_timesteps
-        prev_t = t - self.config.num_train_timesteps // num_inference_steps
-        alpha_prod_t = self.alphas_cumprod[t]
-        alpha_prod_t_prev = self.alphas_cumprod[prev_t] if prev_t >= 0 else self.one
-        current_beta_t = 1 - alpha_prod_t / alpha_prod_t_prev
-
-        # For t > 0, compute predicted variance βt (see formula (6) and (7) from https://arxiv.org/pdf/2006.11239.pdf)
-        # and sample from it to get previous sample
-        # x_{t-1} ~ N(pred_prev_sample, variance) == add variance to pred_sample
-        variance = (1 - alpha_prod_t_prev) / (1 - self.alphas_cumprod[-1]) * current_beta_t
-
-        if variance_type is None:
-            variance_type = self.config.variance_type
-
-        # hacks - were probably added for training stability
-        if variance_type == "fixed_small":
-            variance = torch.clamp(variance, min=1e-20)
-        # for rl-diffuser https://arxiv.org/abs/2205.09991
-        elif variance_type == "fixed_small_log":
-            variance = torch.log(torch.clamp(variance, min=1e-20))
-            variance = torch.exp(0.5 * variance)
-        elif variance_type == "fixed_large":
-            variance = current_beta_t
-        elif variance_type == "fixed_large_log":
-            # Glide max_log
-            variance = torch.log(current_beta_t)
-        elif variance_type == "learned":
-            return predicted_variance
-        elif variance_type == "learned_range":
-            min_log = torch.log(variance)
-            max_log = torch.log(self.betas[t])
-            frac = (predicted_variance + 1) / 2
-            variance = frac * max_log + (1 - frac) * min_log
-
-        return variance
 
     def batch_step_no_noise(
         self,
